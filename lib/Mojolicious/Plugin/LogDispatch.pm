4873e9df33cedb3a35a0f6b3df731b40c4877c11
jonasbn <jonasbn@gmail.com>
2016-05-28 22:29:34 +0200

    Initial tests (lifted) pass, MUCH to be done

--- lib/Mojolicious/Plugin/LogDispatch.pm
+++ lib/Mojolicious/Plugin/LogDispatch.pm
@@ -2,9 +2,12 @@ package Mojolicious::Plugin::LogDispatch;
 
 use strict;
 use warnings;
-use Mojo::Base 'Mojolicious::Plugin';
+use Mojo::Base qw(Mojolicious::Plugin);
+use base qw(Mojo::Log);
 
 use Log::Dispatch;
+use Log::Dispatch::File;
+use Log::Dispatch::Screen;
 
 our $VERSION = '0.01';
 
@@ -20,11 +23,59 @@ sub register {
             $level = lc $level;
             return $self unless $level && $self->is_level($level);
 
+            # We define a default log handle until told otherwise
             $self->handle->log( 'level' => $level, 'message' => @msgs );
+
             return $self;
         }
     );
 }
+
+__PACKAGE__->attr(
+    'handle' => sub {
+        my $self = shift;
+                my $dispatcher;
+
+                $self->path('/tmp/logging.log');
+                $self->level('warning');
+
+                if ($self->callbacks)
+                {
+                        
+                         $dispatcher = Log::Dispatch->new(callbacks => $self->callbacks);      
+                }
+                else
+                {
+                        
+                        $dispatcher = Log::Dispatch->new(@_);
+                }
+                 
+                if ($self->path)
+                {
+        
+                         $dispatcher->add(Log::Dispatch::File->new( 'name'      => '_default_log_obj',
+                                                                            'min_level' => $self->level,
+                                                                            'filename'  => $self->path,
+                                                                            'mode'      => 'append' )
+                                                                                                );                         
+                         
+                }
+                else
+                {
+        
+                        # Create a logging object that will log to STDERR by default
+                        $dispatcher->add(Log::Dispatch::Screen->new(      'name'      => '_default_log_obj',
+                                                                                'min_level' => $self->level,
+                                                                                'stderr'    => 1 )
+                                                                                                        );              
+                }
+
+                return $dispatcher;
+    }
+);
+ 
+__PACKAGE__->attr('callbacks');
+__PACKAGE__->attr('remove_default_log_obj' => 1);
 
 #some methods from Log::Dispatch
 sub add {
@@ -36,7 +87,26 @@ sub add {
     return $l;
 }
 
-sub remove { return shift->handle->remove(@_) }
+sub dispatcher { return shift->handle }
+
+sub log {
+    my ( $self, $level, @msgs ) = @_;
+
+    # Check log level
+    $level = lc $level;
+    return $self unless $level && $self->is_level($level);
+
+    $self->handle->log( 'level' => $level, 'message' => @msgs );
+    return $self;
+}
+
+sub remove         { return shift->handle->remove(@_) }
+sub output         { return shift->handle->output(@_) }
+sub would_log      { return shift->handle->would_log(@_) }
+sub log_to         { return shift->handle->log_to(@_) }
+sub level_is_valid { return shift->handle->level_is_valid(@_) }
+sub log_and_die    { return shift->handle->log_and_die(@_) }
+sub log_and_croak  { return shift->handle->log_and_croak(@_) }
 
 sub fatal     { shift->log( 'emergency', @_ ) }
 sub emergency { shift->log( 'emergency', @_ ) }
@@ -45,6 +115,7 @@ sub critical  { shift->log( 'critical',  @_ ) }
 sub warning   { shift->log( 'warning',   @_ ) }
 sub warn      { shift->log( 'warning',   @_ ) }
 sub notice    { shift->log( 'notice',    @_ ) }
+sub debug    { shift->log( 'debug',    @_ ) }
 
 #short alias syslog style
 sub err   { shift->log( 'error',     @_ ) }
@@ -53,6 +124,7 @@ sub emerg { shift->log( 'emergency', @_ ) }
 
 sub is_level {
     my ( $self, $level ) = @_;
+
     return 0 unless $level;
     $level = lc $level;
     return $self->would_log($level);
@@ -68,7 +140,9 @@ sub is_notice    { shift->is_level('notice') }
 sub is_err       { shift->is_level('error') }
 sub is_crit      { shift->is_level('critical') }
 sub is_emerg     { shift->is_level('emergency') }
-
+sub is_debug     { shift->is_level('debug') }
+sub is_info     { shift->is_level('info') }
+sub is_error     { shift->is_level('error') }
 1;
 
 =encoding utf8
@@ -129,7 +203,5 @@ Jonas B. Nielsen, (jonasbn) - C<< <jonasbn@cpan.org> >>
 Mojolicious-Plugin-LogDispatch is (C) by Jonas B. Nielsen, (jonasbn) 2016
 
 Mojolicious-Plugin-LogDispatch is released under the Artistic License 2.0
-
-=cut
 
 =cut
